bd73705358affacc6eb540402088956f234b96de
Manuel Kipfer <manuel.kipfer@swisscom.com>
2017-10-06 18:14:33 +0200

    - Add more tests for MediaStream - Adjust MediaStream API

--- test/stream/MediaStream.js
+++ test/stream/MediaStream.js
@@ -1,3 +1,7 @@
+const fs = require('fs-extra');
+const PassThrough = require('stream').PassThrough;
+const ReadableStream = require('stream').ReadableStream;
+const WriteableStream = require('stream').WriteableStream;
 const assert = require('assertthat');
 const Client = require('../../lib/client/Client');
 const Playlist = require('../../lib/playlist/Playlist');
@@ -5,6 +9,8 @@ const PlaylistItem = require('../../lib/playlist/Item');
 const MediaStream = require('../../lib/stream/MediaStream');
 const MediaSource = require('../../lib/mediasource/MediaSource');
 const MediaSourceYoutube = require('../../lib/mediasource/Youtube');
+const MultiStream = require('multistream');
+const Playable = require('../../lib/playable/Playable');
 
 describe('MediaStream Tests', () => {
   describe('Test constructor', () => {
@@ -207,23 +213,128 @@ describe('MediaStream Tests', () => {
   });
 
   describe('Test registerClient method', () => {
+    const playlist = new Playlist({});
+    const decoder = {};
+    const encoder = {};
+    const stream = new MediaStream(playlist, decoder, encoder);
+
     it('Must throw an error if the given client is not an instance of Client', (done) => {
       assert.that(() => {
-        const playlist = new Playlist({});
-        const decoder = {};
-        const encoder = {};
-        const stream = new MediaStream(playlist, decoder, encoder);
         stream.registerClient({});
       }).is.throwing('The given client must be an instance of Client!');
+
+      done();
+    });
+
+    it('Must add the given client to clients', (done) => {
+      const client = new Client();
+      stream.registerClient(client);
+
+      assert.that(stream.hasClient(client.getId())).is.true();
+      assert.that(stream.getClient(client.getId())).is.sameAs(client);
+
+      done();
+    });
+
+    it('Must return the same MediaStream instance (fluent interface)', (done) => {
+      const client = new Client();
+      const ret = stream.registerClient(client);
+
+      assert.that(ret).is.sameAs(stream);
 
       done();
     });
   });
 
   describe('Test unregisterClient method', () => {
+    const playlist = new Playlist({});
+    const decoder = {};
+    const encoder = {};
+    const stream = new MediaStream(playlist, decoder, encoder);
+
     it('Must throw an error if no such client is registered', (done) => {
+      assert.that(() => {
+        stream.unregisterClient('test');
+      }).is.throwing(`No client with id test registered!`);
 
       done();
     });
+
+    it('Must remove the client when called with an id', (done) => {
+      const client = new Client();
+      stream.registerClient(client);
+      stream.unregisterClient(client.getId());
+      assert.that(stream.hasClient(client.getId())).is.false();
+
+      done();
+    });
+
+    it('Must remove the client when called with a client instance', (done) => {
+      const client = new Client();
+      stream.registerClient(client);
+      stream.unregisterClient(client);
+      assert.that(stream.hasClient(client.getId())).is.false();
+
+      done();
+    });
+  });
+
+  describe('Test getStream method', () => {
+    const playlist = new Playlist({});
+    const decoder = {};
+    const encoder = {};
+    const stream = new MediaStream(playlist, decoder, encoder);
+
+    it('Must throw an error if stream was not started', (done) => {
+      assert.that(() => {
+        stream.getMultiStream();
+      }).is.throwing('No multistream instance available! Did you forget to start the stream?');
+
+      done();
+    });
+  });
+
+  describe('Test play method', () => {
+    const playlist = new Playlist({}, {repeat: true});
+    const item1 = new PlaylistItem({mediasource: 'test'});
+    const item2 = new PlaylistItem({mediasource: 'test'});
+    playlist.add(item1)
+    .add(item2);
+
+    const decoder = new PassThrough();
+    const encoder = new PassThrough();
+    const stream = new MediaStream(playlist, decoder, encoder);
+
+    // MediaSource mock
+    const playable = new Playable();
+    const mediasource = new MediaSource('test');
+    mediasource.get = () => {
+      return playable;
+    };
+
+    stream.registerMediaSource(mediasource);
+
+    it('Next must return an instance of Playable', (done) => {
+      stream.next((err, ret) => {
+        assert.that(ret).is.instanceOf(Playable);
+
+        done();
+      });
+    });
+
+    it('Client must receive data sent to playable', (done) => {
+      const client = new Client();
+      client.write = (chunk) => {
+        assert.that(chunk.toString()).is.equalTo('test');
+
+        done();
+      };
+
+      stream.registerClient(client);
+      stream.start();
+
+      playable.write('test');
+    });
+
   });
 });
